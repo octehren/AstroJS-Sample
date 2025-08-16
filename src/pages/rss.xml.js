6b1ac70018f39c4afa05ac3d83eb21482d8a82b4
oivatom <ehren.dev.mail@gmail.com>
2025-08-16 06:55:10 +0200

    updated rss feed to use collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("blog");
   return rss({
     title: 'CHANGE THIS TO FEED TITLE',
     description: 'CHANGE THIS TO FEED DESCRIPTION',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
   });
 }
